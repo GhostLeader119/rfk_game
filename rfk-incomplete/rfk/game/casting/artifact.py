6751b829518fb575dd6c4d21dd76e3709d68303e
GhostLeader119 <97194393+GhostLeader119@users.noreply.github.com>
2022-02-14 10:48:17 -0700

    Add files via upload

---
+++ rfk-incomplete/rfk/game/casting/artifact.py
@@ -0,0 +1,18 @@
+from game.casting.actor import Actor
+
+# TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
+class Artifact(Actor):
+    
+    def __init__(self):
+        super().__init__()
+        self._message = ""
+
+    def get_message(self):
+        
+        return self._message
+
+    def set_message(self,message_input):
+        self._message = message_input
+
+
+   
